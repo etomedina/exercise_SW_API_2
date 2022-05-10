3d67384d29edf0d95f3fd5156d81c762bdba3641
etomedina <hmedina@LAPTOP-84M4TVEB.localdomain>
2022-05-10 19:58:29 -0400

    Primer commit, chequeo ultimos endpoints

--- src/main.py
+++ src/main.py
@@ -2,13 +2,16 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import requests
+import json
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet, Vehicle, Favorite
+from sqlalchemy import select
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +33,268 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# @app.route('/user', methods=['GET'])
+# def handle_hello():
+
+#     response_body = {
+#         "msg": "Hello, this is your GET /user response "
+#     }
+
+#     return jsonify(response_body), 200
+
+BASE_URL="https://www.swapi.tech/api"
+
+#POBLAR LISTA DE CHARACTERS
+
+@app.route('/populate-characters',methods=["POST"])
+def populate_characters():
+    response=requests.get(
+        f"{BASE_URL}{'/people'}"
+    )
+    #cuerpo con todos los personajes
+    body=response.json()
+    all_characters=[]
+
+    #ciclo recorrer la respuesta y obtener detalles de cada uno    
+    for result in body['results']:
+        response=requests.get(result['url'])
+        body=response.json()
+
+    #se agregan las propiedades de cada character a la lista
+        character_insert=body['result']['properties']
+        character_insert['uid']=body['result']['uid']
+        character_insert['description']=body['result']['description']
+        all_characters.append(character_insert)
+    instances=[]
+    #recorremos la lista con todos los personajes y creamos la instancia
+    for character in all_characters:
+        instance=Character.create(character)
+    #agregamos el OBJETO character a la lista instance
+        instances.append(instance)
+
+    return jsonify(list(map(
+        lambda inst: inst.serialize(), 
+        instances
+    ))),200
+
+#POBLAR LISTA DE PLANETS
+@app.route('/populate-planets',methods=["POST"])
+def populate_planets():
+    response=requests.get( f"{BASE_URL}{'/planets'}")
+    #cuerpo con todos los planets
+    body=response.json()
+    all_planets=[]
+
+    #ciclo recorrer la respuesta y obtener detalles de cada uno    
+    for result in body['results']:
+        response=requests.get(result['url'])
+        body=response.json()
+
+    #se agregan las propiedades de cada planet a la lista
+        planet_new=body['result']['properties']
+        planet_new['uid']=body['result']['uid']
+        planet_new['description']=body['result']['description']
+        all_planets.append(planet_new)
+    instances=[]
+    #recorremos la lista con todos los planets y creamos la instancia
+    for planet in all_planets:
+        instance=Planet.create(planet)
+    #agregamos el OBJETO character a la lista instance
+        instances.append(instance)
+
+    return jsonify(list(map(
+        lambda inst: inst.serialize(), 
+        instances
+    ))),200
+
+
+#POBLAR LISTA DE VEHICLES
+@app.route('/populate-vehicles',methods=["POST"])
+def populate_vehicles():
+    response=requests.get( f"{BASE_URL}{'/vehicles'}" )
+    #cuerpo con todos los vehicles
+    body=response.json()
+    all_vehicles=[]
+
+    #ciclo recorrer la respuesta y obtener detalles de cada uno    
+    for result in body['results']:
+        response=requests.get(result['url'])
+        body=response.json()
+
+    #se agregan las propiedades de cada vehicle a la lista
+        vehicle_new=body['result']['properties']
+        vehicle_new['uid']=body['result']['uid']
+        vehicle_new['description']=body['result']['description']        
+        all_vehicles.append(vehicle_new)
+    instances=[]
+    #recorremos la lista con todos los personajes y creamos la instancia
+    for vehicle in all_vehicles:
+        instance=Vehicle.create(vehicle)
+    #agregamos el OBJETO character a la lista instance
+        instances.append(instance)
+
+    return jsonify(list(map(
+        lambda inst: inst.serialize(), 
+        instances
+    ))),200
+
+#1. obtener people
+@app.route('/characters', methods=['GET'])
+def get_all_people():
+    characters= Character.query.all()
+    list_people= list(map(
+        lambda persona: persona.serialize(), characters))
+    return jsonify(list_people),200
+
+
+#2. obtener people/especifica
+@app.route('/characters/<int:id>', methods=['GET'])
+def more_details_person(id):
+    #buscar en base de datos al pesonaje cuya id corresponde a la suya
+    character_esp = Character.query.get(id)
+    if isinstance(character_esp,Character):
+        #enviar vista detallada del personaje 
+        dictionary = character_esp.serialize()
+        return jsonify(dictionary),200
+    return 404
+
+#3. obtener planetas
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    planets= Planet.query.all()
+    list_planets= list(map(
+        lambda planeta: planeta.serialize(), planets))
+    return jsonify(list_planets),200
+
+
+#4. obtener planetas/especificos
+@app.route('/planets/<int:id>', methods=['GET'])
+def more_details_planet(id):
+    #buscar en base de datos del planeta cuya id corresponde a la suya
+    planet = Planet.query.get(id)
+    if isinstance(planet,Planet):
+        #enviar vista detallada del planeta
+        dictionary = planet.serialize()
+        return jsonify(dictionary),200
+    return 404
+
+#5. obtener usuarios
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_all_user():
+    users= User.query.all()
+    list_users= list(map(
+        lambda item: item.serialize(), users))
+    return jsonify(list_users),200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+#5.1. Postear usuarios
+@app.route('/user', methods=['POST'])
+def add_one_user():
+    dictionary={}
+    body = request.json
+    decoded_object = json.loads(request.data)
+    user = User.create(
+        email=body["email"],
+        name=body["name"],
+        lastname=body["lastname"],
+        password=body["password"],
+        is_active= body["is_active"],
+        )
+    dictionary = user.serialize()
+    return jsonify(dictionary), 201
 
-    return jsonify(response_body), 200
+#5.2. Delete usuarios
+@app.route('/user/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user= User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"msg": f"user id:{id} has been deleted succesfully"}),204
+
+#6. obtener favoritos
+@app.route('/favorite', methods=['GET'])
+def get_favorite():
+    favorites= Favorite.query.all()
+    list_favorites= list(map(
+        lambda item: item.serialize(), favorites))
+    return jsonify(list_favorites),200
+
+#6.1. Obtener favoritos/usuario
+@app.route('/favorite/user/<int:id>', methods=['GET'])
+def get_user_favorite(id):
+    all_favorite_one_user= Favorite.query.filter(Favorite.id_user==id).all()
+    dictionary_favorite_user=[]
+    for general_fav in all_favorite_one_user:
+        dictionary_favorite_user.append(general_fav.serialize())
+    return jsonify(dictionary_favorite_user),200
+
+#7. postear favorito, planeta por su id
+@app.route('/favorite/planet/<int:planet_fav>', methods=['POST'])
+def add_new_favorite_planet(planet_fav):
+    dictionary={}
+    request_body= request.json
+    planet_favorite = Favorite.create(
+        id_user= request_body['id_user'],
+        planet_fav= request_body['planet_fav']
+    )
+    dictionary = Favorite.serialize()
+    return jsonify(dictionary), 201
+
+#8. postear favorito, people por su id
+@app.route('/favorite/character/<int:character_fav>', methods=['POST'])
+def add_new_favorite_character(character_fav):
+    dictionary={}
+    request_body= request.json
+    planet_favorite = Favorite.create(
+        id_user= request_body['id_user'],
+        character_fav= request_body['character_fav']
+    )
+    dictionary = Favorite.serialize()
+    return jsonify(dictionary), 201
+
+#9. borrar favorito, planeta por su id
+@app.route('/favorite/planet/', methods=['DELETE'])
+def delete_fav_planet():
+    stmt = select(Favorite).where(Favorite.id_user == "1" and Favorite.planet_fav =="2")
+    favorite1= Favorite.query.get(stmt[id])
+    db.session.delete(favorite1)
+    db.session.commit()
+    return jsonify({"msg": f"favorite id:{stmt[id]} has been deleted succesfully"}),204
+
+# def delete_fav_planet(id):
+    # id=request.args.get("id", default="", type=str)
+    # id=request.args.get("id")
+    # id_planet=request.args.get("planet_fav", default="", type=str)
+    # print(id_user,id_planet)
+
+    # query2= Favorite.query.filter(Favorite.planet_fav==id_planet).all()
+    # stmt = select(Favorite).where(favorite.id_user == "1" and favorite.planet_fav =="2")
+    # stmt = select(user_table).where(user_table.c.name == 'spongebob')
+
+    # query = Favorite.query.get((id))
+    # query1=db.session.query(Favorite).get((id))
+    # if query1 is None:
+    #     return jsonify({
+    #         "msg": "not found"
+    #         }), 404
+    # db.session.delete(query1)
+    # db.session.commit()
+    # return print( { "msg": "favorite id{id_planet}was deleted "}), 204 
+
+#10. borrar favorito, people por su id
+@app.route('/favorite/character/<int:character_fav>', methods=['DELETE'])
+def delete_fav_character(character_fav):
+    id_user=request.args.get("id_user", default="", type=str)
+    id_people=request.args.get("character_fav", default="", type=str)
+    query = Favorite.query.get((id_user,id_people))
+    query1=db.session.query(Favorite).get((id_user,id_people))
+    if query is None:
+        return jsonify({
+            "msg": "not found"
+            }), 404
+    db.session.delete(query1)
+    db.session.commit()
+    return print( { "msg": "favorite id{id_people}was deleted "}), 204 
+     
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,9 +4,12 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    lastname = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    is_active = db.Column(db.String(80))
+    id_favorite=db.relationship('Favorite',backref='user',)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -15,5 +18,220 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "lastname": self.lastname,
+            "password": self.password,
+            "is_active": self.is_active,
             # do not serialize the password, its a security breach
         }
+
+    @classmethod
+    def create(cls, **data):
+        #Crear Instancia
+        instance=cls(**data)
+        if (not isinstance(instance,cls)):
+            print("FALLA EL CONSTRUCTOR")
+            return None
+        #guardar en bdd
+        db.session.add(instance)
+        try:
+            db.session.commit()
+            return instance
+        except Exception as error:
+            print('FALLA BDD')
+            db.session.rollback()
+            #return None
+            raise Exception(error.args)
+
+class item(db.Model):
+    __abstract__ = True
+    # Here we define db.Columns for the table person
+    # Notice that each db.Column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    url= db.Column(db.String(250), nullable=True)
+    uid=db.Column(db.Integer,nullable=True)
+    description=db.Column(db.String,nullable=True)
+
+class Character(item):
+    # Here we define db.Columns for the table address.
+    # Notice that each db.Column is also a normal Python instance attribute.
+    birth_year = db.Column(db.String(250),nullable=True)
+    eye_color = db.Column(db.String(250),nullable=True)
+    gender = db.Column(db.String(250),nullable=True)
+    height = db.Column(db.Numeric(precision=6, scale=2),nullable=True)
+    mass = db.Column(db.Numeric(precision=6, scale=2),nullable=True)
+    starship = db.Column(db.String(250),nullable=True)
+    vehicles = db.Column(db.String(250),nullable=True)
+    characters_favorites=db.relationship("Favorite",backref="character")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "uid":self.uid,
+            "name": self.name,
+            "eye_color":self.eye_color,
+            "birth_year":self.birth_year,
+            "gender":self.gender,
+            "heigth":self.height,
+            "mass":self.mass,
+            "description":self.description
+            }
+
+    def __init__(self,**kwargs):
+        for (key, value) in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key,  value)
+                
+
+    @classmethod
+    def create(cls, data):
+        #Crear Instancia
+        instance=cls(**data)
+        if (not isinstance(instance,cls)):
+            print("FALLA EL CONSTRUCTOR")
+            return None
+        #guardar en bdd
+        db.session.add(instance)
+        try:
+            db.session.commit()
+            return instance
+        except Exception as error:
+            print('FALLA BDD')
+            db.session.rollback()
+            #return None
+            raise Exception(error.args)
+        
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return True
+
+class Planet(item):
+    diameter=db.Column(db.String(250))
+    rotation_period=db.Column(db.String(250))
+    orbital_period=db.Column(db.String(250))
+    gravity=db.Column(db.String(250))
+    population=db.Column(db.String(250))
+    climate=db.Column(db.String(250))
+    residents=db.Column(db.String(250))
+    terrain=db.Column(db.String(250))
+    planets_favorites=db.relationship("Favorite",backref="planet")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "uid":self.uid,
+            "name": self.name,
+            "diameter":self.diameter,
+            "rotation_period":self.rotation_period,
+            "orbital_period":self.orbital_period,
+            "description":self.description
+            }
+
+    def __init__(self,**kwargs):
+        for (key, value) in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key,  value)
+
+    @classmethod
+    def create(cls, data):
+        #Crear Instancia
+        instance=cls(**data)
+        if (not isinstance(instance,cls)):
+            print("FALLA EL CONSTRUCTOR")
+            return None
+        #guardar en bdd
+        db.session.add(instance)
+        try:
+            db.session.commit()
+            return instance
+        except Exception as error:
+            print('FALLA BDD')
+            db.session.rollback()
+            #return None
+            raise Exception(error.args)
+        
+    
+
+class Vehicle(item):
+    vehicle_class=db.Column(db.String(250))
+    manufacturer=db.Column(db.String(250))
+    length=db.Column(db.String(250))
+    cost_in_credits=db.Column(db.String(250))
+    crew=db.Column(db.String(250))
+    passengers=db.Column(db.String(250))
+    cargo_capacity=db.Column(db.String(250))
+    consumable=db.Column(db.String(250))
+    pilots=db.Column(db.String(500))
+    vehicles_favorites=db.relationship("Favorite",backref="vehicle")
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "uid":self.uid,
+            "name": self.name,
+            "manufacturer":self.manufacturer,
+            "length":self.length,
+            "passengers":self.passengers,
+            "description":self.description
+            }
+
+    def __init__(self,**kwargs):
+        for (key, value) in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key,  value)
+
+    @classmethod
+    def create(cls, data):
+        #Crear Instancia
+        instance=cls(**data)
+        if (not isinstance(instance,cls)):
+            print("FALLA EL CONSTRUCTOR")
+            return None
+        #guardar en bdd
+        db.session.add(instance)
+        try:
+            db.session.commit()
+            return instance
+        except Exception as error:
+            print('FALLA BDD: ', error.args)
+            db.session.rollback()
+            #return None
+            raise Exception(error.args)
+        
+class Favorite(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    id_user=db.Column(db.Integer,db.ForeignKey('user.id'))
+    character_fav=db.Column(db.Integer,db.ForeignKey('character.id'))
+    planet_fav=db.Column(db.Integer,db.ForeignKey('planet.id'))
+    vehicle_fav=db.Column(db.Integer,db.ForeignKey('vehicle.id'))
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id_user": self.id_user,
+            "character_fav": self.character_fav,
+            "planet_fav": self.planet_fav,
+            "vehicle_fav": self.vehicle_fav
+        }  
+
+    @classmethod
+    def create(cls, data):
+        #Crear Instancia
+        instance=cls(**data)
+        if (not isinstance(instance,cls)):
+            print("FALLA EL CONSTRUCTOR")
+            return None
+        #guardar en bdd
+        db.session.add(instance)
+        try:
+            db.session.commit()
+            return instance
+        except Exception as error:
+            print('FALLA BDD: ', error.args)
+            db.session.rollback()
+            #return None
+            raise Exception(error.args)
